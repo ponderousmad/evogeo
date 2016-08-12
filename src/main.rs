b13ffe05ef971cadd3ecca90e4de110e1ebe88fc
Adrian Smith <agnomen@gmail.com>
2016-08-12 10:33:34 -0400

    Remove point from evogeo.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
-    w: f64
-}
-
 fn main() {
-    let origin = Point { x: 0.0, y: 0.0, z: 0.0, w: 1.0 }; // origin: Point
-
-    println!("The origin is at ({}, {}, {}, {})", origin.x, origin.y, origin.z, origin.w);
+    println!("Nothing here yet.");
 }
